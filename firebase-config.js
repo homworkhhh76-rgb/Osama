aefc6d8222c35aa4e307b33e28de622dac8999c6
homworkhhh76-rgb <homworkhhh76@gmail.com>
2026-05-03 00:04:09 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBIV6A9RkGvMaqMVWQtifhnVdS4NeMdmr0",
+    authDomain: "oskar-975dd.firebaseapp.com",
+    databaseURL: "https://oskar-975dd-default-rtdb.firebaseio.com",
+    projectId: "oskar-975dd",
+    storageBucket: "oskar-975dd.firebasestorage.app",
+    messagingSenderId: "1050593646706",
+    appId: "1:1050593646706:web:3650fbb3173fda6eda8aeb",
+    measurementId: "G-PGJR0V564L"
+  },
+
+  firebaseRoot: "cashier_units_pro_oskar_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_oskar_885766842"
+  ]
+};
